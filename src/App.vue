f3fd27ae9bd34c3fbfc22fcb5a8f3496ffa3d9e3
jsntlr1 <147086859+jsntlr1@users.noreply.github.com>
2024-09-20 23:34:12 -0500

    Source files
    Necessary vue files added

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="app">
+        <EventManagementForm />
+        <VolunteerMatchingForm />
+    </div>
+</template>
+
+<script setup>
+import EventManagementForm from './components/EventManagementForm.vue'
+import VolunteerMatchingForm from './components/VolunteerMatchingForm.vue'
+</script>
